0dde9d013f3536041204d9b8e99bcbca4e26be92
Muhmud Ahmad <muhmud.ahmad@toptal.com>
2025-01-22 12:30:31 +0300

    more stuff

--- lua/user/keys.lua
+++ lua/user/keys.lua
@@ -157,16 +157,4 @@ vim.cmd [[
   xmap <silent> ib <Plug>CamelCaseMotion_ib
   omap <silent> ie <Plug>CamelCaseMotion_ie
   xmap <silent> ie <Plug>CamelCaseMotion_ie
-
-  let g:surround_no_mappings = 1
-  nmap ds       <Plug>Dsurround
-  nmap cs       <Plug>Csurround
-  nmap cS       <Plug>CSurround
-  nmap ys       <Plug>Ysurround
-  nmap yS       <Plug>YSurround
-  nmap yss      <Plug>Yssurround
-  nmap ySs      <Plug>YSsurround
-  nmap ySS      <Plug>YSsurround
-  xmap gs       <Plug>VSurround
-  xmap gS       <Plug>VgSurround
 ]]
